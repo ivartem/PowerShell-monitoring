b5dfba20df920a08bf3b37ca9bd172be440c3150
Artem Ivanov <ivartem@gmail.com>
2017-09-21 12:48:44 +0300

    Network monitoring the availability of servers using PowerShell script

---
+++ monitoring.ps1
@@ -0,0 +1,47 @@
+﻿<#
+ # Nnetwork monitoring with powershell
+#>
+
+Function monitoring {
+  [CmdletBinding()]
+    Param(
+      $ComputerName    
+    )
+
+  #add to csv file 'namepc,ip'"
+  $ComputerName = Import-Csv "c:\monitoringpc.csv"
+  $Count = @{}
+  $Countnok = @{} 
+
+  while ($True){
+
+    foreach ($Computeritem in $ComputerName){
+
+      $Computer = $Computeritem.ip
+      $ComputerITName = $Computeritem.Name
+
+          if (($Count.$Computer) -eq $null){
+           $Count.add($Computer,"True") 
+          }
+          if (($Countnok.$Computer) -eq $null){
+            $Countnok.add($Computer,"True") 
+          }
+
+      $Countnok.$Computer = $Count.$Computer
+      $Count.$Computer = Test-Connection -BufferSize 32 -Count 4 -Delay 4 -ComputerName $Computer -Quiet
+
+      if (($Countnok.$Computer) -eq ($Count.$Computer)){
+       # NOTHING
+      }
+      elseif ($Count.$Computer){
+        Write-Host -Object "$ComputerITName ($Computer) - is UP $(Get-Date -UFormat '%Y/%m/%d %H:%M:%S')"
+      }
+      else{
+        Write-Host -Object "$ComputerITName ($Computer) - is DOWN $(Get-Date -UFormat '%Y/%m/%d %H:%M:%S')"
+      }
+      Start-Sleep -Seconds 1
+    }
+  }
+}
+
+  monitoring
---
+++ monitoring_mail.ps1
@@ -0,0 +1,49 @@
+﻿<#
+ # Nnetwork monitoring with send e-mail notifications
+#>
+
+Function monitoring_mail {
+  [CmdletBinding()]
+    Param(
+      # Enter local e-mail server 
+      $PSEmailServer = "mail.example.local",
+      $ComputerName    
+    )
+
+  #add to csv file 'namepc,ip'"
+  $ComputerName = Import-Csv "c:\monitoringpc.csv"
+  $Count = @{}
+  $Countnok = @{} 
+
+  while ($True){
+
+    foreach ($Computeritem in $ComputerName){
+
+      $Computer = $Computeritem.ip
+      $ComputerITName = $Computeritem.Name
+
+          if (($Count.$Computer) -eq $null){
+           $Count.add($Computer,"True") 
+          }
+          if (($Countnok.$Computer) -eq $null){
+            $Countnok.add($Computer,"True") 
+          }
+
+      $Countnok.$Computer = $Count.$Computer
+      $Count.$Computer = Test-Connection -BufferSize 32 -Count 4 -Delay 4 -ComputerName $Computer -Quiet
+
+      if (($Countnok.$Computer) -eq ($Count.$Computer)){
+       # NOTHING
+      }
+      elseif ($Count.$Computer){
+        Send-MailMessage -From "networkmon@example.com<networkmon@example.com>" -To "<admin@example.com>"  -Encoding UTF8 -Subject "$ComputerITName ($Computer) - is UP $(Get-Date -UFormat '%Y/%m/%d %H:%M:%S')" -Body "$ComputerITName ($Computer) - is UP $(Get-Date -UFormat '%Y/%m/%d %H:%M:%S')" -BodyAsHtml
+      }
+      else{
+        Send-MailMessage -From "networkmon@example.com<networkmon@example.com>" -To "<admin@example.com>" -Encoding UTF8 -Subject "$ComputerITName ($Computer) - is DOWN $(Get-Date -UFormat '%Y/%m/%d %H:%M:%S')" -Body "$ComputerITName ($Computer) - is DOWN $(Get-Date -UFormat '%Y/%m/%d %H:%M:%S')" -BodyAsHtml
+      }
+      Start-Sleep -Seconds 1
+    }
+  }
+}
+
+  monitoring_mail
